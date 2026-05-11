7f3756aa86cf7407452c93ec56caa9567471fc21
xieqiyong <xieqiyong66@gmail>
2026-05-11 15:59:27 +0800

    feat#新增二方包

--- expert-web/src/main/java/com/databuff/digitalexpert/DigitalExpertApplication.java
+++ expert-web/src/main/java/com/databuff/digitalexpert/DigitalExpertApplication.java
@@ -1,7 +1,6 @@
 package com.databuff.digitalexpert;
 
 import com.databuff.digitalexpert.config.ExpertProperties;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(ExpertProperties.class)
-@MapperScan({"com.databuff.digitalexpert.dao.mapper"})
 public class DigitalExpertApplication {
 
     public static void main(String[] args) {
---
+++ expert-web/src/main/java/com/databuff/digitalexpert/config/MybatisConfig.java
@@ -0,0 +1,37 @@
+package com.databuff.digitalexpert.config;
+
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.databuff.digitalexpert.dao.mapper")
+public class MybatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+
+        MybatisConfiguration configuration = new MybatisConfiguration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        factoryBean.setConfiguration(configuration);
+
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setBanner(false);
+        factoryBean.setGlobalConfig(globalConfig);
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
